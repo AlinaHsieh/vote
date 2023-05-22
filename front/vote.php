730923e094da08efab8e2b13bab5d22017d71db7
Alina <ahsieh74@gmail.com>
2023-05-22 11:54:19 +0800

    vote投票系統

--- front/vote.php
+++ front/vote.php
@@ -4,24 +4,33 @@
 $topic=$pdo->query("select * from `topics` where `id`='{$_GET['id']}'")->fetch(PDO::FETCH_ASSOC);
 $options=$pdo->query("select * from `options` where `subject_id`='{$_GET['id']}'")->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <h2><?=$topic['subject'];?></h2>
 <form action="./api/vote.php" method="post">
 <ul>
 <?php
 foreach($options as $idx => $opt){
     echo "<li>";
-    echo "<input type='radio' name='desc' value='{$opt['id']}'>";
+    switch($topic['type']){
+        case 1:
+            echo "<input type='radio' name='desc' value='{$opt['id']}'>";                
+        break;
+        case 2:        
+            echo "<input type='checkbox' name='desc[]' value='{$opt['id']}'>";
+        break;
+    }
+    
     echo "<span>".($idx+1).". </span>";
     echo "<span>{$opt['description']}</span>";
-    echo "</li>" ;
+    echo "</li>";
 }
-
 ?>
 </ul>
+
 <div>
-    <input type="hidden" name="subject_id" value="<?=$_GET['id']?>">
+    <input type="hidden" name="subject_id" value="<?=$_GET['id'];?>">
     <input type="submit" value="投票">
     <input type="button" value="取消">
-
 </div>
+
 </form>
