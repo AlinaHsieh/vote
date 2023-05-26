e2f1c057a4bc6f38e07cad6312967bee64efb406
Alina <ahsieh74@gmail.com>
2023-05-26 13:34:25 +0800

    投票系統20230526

--- api/login.php
+++ api/login.php
@@ -4,8 +4,23 @@ $sql="select count(*) from `members` where `acc`='{$_POST['acc']}' && `pw`='{$_P
 
 $chk=$pdo->query($sql)->fetchColumn();
 
+
 if($chk){
+    
+    $sql_pr="select `pr` from `members` where `acc`='{$_POST['acc']}' && `pw`='{$_POST['pw']}'";
+    
+    $pr=$pdo->query($sql_pr)->fetchColumn();
+
     $_SESSION['login']=$_POST['acc'];
+    
+    $_SESSION['pr']=$pr;
+
+    if(isset($_SESSION['position'])){
+        header("location:".$_SESSION['position']);
+        unset($_SESSION['position']);
+        exit();
+    }
+
     header("location:../index.php");
 }else{
     header("location:../index.php?do=login&error=1");
